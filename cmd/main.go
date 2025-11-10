137b8988aa0ba80f1a31092dbf4eb60cf5e5a2dd
ArteShow <sokartemax@gmail.com>
2025-11-10 23:28:11 +0100

    added some structure and set up logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,8 @@
-package cmd
+package main
+
+import "github.com/ArteShow/ArteDataStorage/pkg/logger"
+
+func main() {
+	logger.Init()
+	logger.Log.Println("Initializing logger")
+}
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,29 @@
 package logger
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
+
+var Log *log.Logger
+
+func Init() {
+	rootDir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("cannot get working directory: %v", err)
+	}
+
+	logsDir := filepath.Join(rootDir, "logs")
+	if err := os.MkdirAll(logsDir, 0755); err != nil {
+		log.Fatalf("cannot create logs directory: %v", err)
+	}
+
+	logFile := filepath.Join(logsDir, "app.log")
+	file, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalf("cannot open log file: %v", err)
+	}
+
+	Log = log.New(file, "", log.Ldate|log.Ltime|log.Lshortfile)
+}
