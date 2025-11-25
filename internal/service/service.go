ac04c6c62e30a4bc636e035380d2dc4b41736614
ArteShow <sokartemax@gmail.com>
2025-11-26 00:12:56 +0100

    Implement CreateFile and DeleteFile functions in service package

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,38 @@
 package service
+
+import (
+	"github.com/ArteShow/ArteDataStorage/internal/repository"
+	"github.com/ArteShow/ArteDataStorage/pkg/logger"
+)
+
+func CreateFile(name, userID, formate, path string) (string, error) {
+	DbEntryId, err := repository.CreateFile(name, userID, formate)
+	if err != nil {
+		logger.Log.Println("service.go: Failed to create an entry in the file db ", err)
+		return "", err
+	}
+
+	err = repository.MoveFile(userID, path, name)
+	if err != nil {
+		logger.Log.Println("service.go: Failed to move the file ", err)
+		return "", err
+	}
+
+	return DbEntryId, nil
+}
+
+func DeleteFile(name, userId, path, dbEntryId string) error {
+	err := repository.DeleteFile(userId, dbEntryId)
+	if err != nil {
+		logger.Log.Println("service.go: Failed to delete the file ", err)
+		return err
+	}
+
+	err = repository.DeleteUserFile(userId, name, path)
+	if err != nil {
+		logger.Log.Println("service.go: Failed to delete the file ", err)
+		return err
+	}
+
+	return nil
+}
